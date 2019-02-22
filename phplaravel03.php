ba326cbce2edaa0b004285499b6e4b58a7acaf33
behike56 <kc7u9k9tim3jfqtrrn3jykgecdwurk@gmail.com>
2019-02-23 00:31:24 +0900

    PHP/Laravel 03:課題

---
+++ phplaravel03.php
@@ -0,0 +1,54 @@
+<?php
+//PHP/Laravel 03 – プログラムの実行を条件分岐構文と繰り返し構文で制御できるようになろう
+//1
+$name = "Masato";
+
+if($name == "Masato"){
+  echo "私は".$name."です";
+  echo "\n";
+}else{
+  echo "あなたの名前ではありません";
+  echo "\n";
+}
+
+//2
+$sum_count = 1;
+$sum_num = 1;
+$end_count = 0;
+
+for($i=1; $i != 100; $i++){
+  $sum_count++;
+  $sum_num = $sum_num + $sum_count;
+  $end_count++;
+  
+  if($end_count==99){
+    echo $sum_num;
+    echo "\n";
+  }
+}
+
+
+//3
+$fruits = array("林檎","梨","苺","桃","蜜柑");
+
+foreach($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+}
+
+//4
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i < $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
+
+?>
